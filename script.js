664e5bc8df92d72346b5fd89cc3bb68b0b3883e0
Len tas <88710364+lentas1@users.noreply.github.com>
2024-06-10 19:30:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+addEventListener("click", function() {
+    var
+          el = document.documentElement
+        , rfs =
+               el.requestFullScreen
+            || el.webkitRequestFullScreen
+            || el.mozRequestFullScreen
+           
+    ;
+    rfs.call(el);
+});
+ 
